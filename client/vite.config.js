4b6a5fd00599e829f6f0c7d53e6a1e3655d27e54
zilsh1 <159803416+zilsh1@users.noreply.github.com>
2024-12-30 18:35:02 +0530

    complete sign up page ui

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3009, // Replace 3001 with your desired port number
+  },
 })
