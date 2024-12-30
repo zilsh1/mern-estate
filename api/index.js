8c4421ebee9c1e1695f59f2ac8e7c9c9565a63a1
zilsh1 <159803416+zilsh1@users.noreply.github.com>
2024-12-30 08:26:09 +0530

    connectd to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import exprees  from 'express'
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+dotenv.config()
+mongoose.connect(process.env.MONGO)
+.then(()=>{console.log("Connected to MongoDB");
+})
+.catch((err)=>{console.log(err);
+})
 const app=exprees()
 app.listen(3000,()=>
 {
